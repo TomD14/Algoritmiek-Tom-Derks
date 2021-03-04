ebac38db55eceb2a9548a7a33a81f1a033ff53db
Tom Derks <459530@student.fontys.nl>
2021-03-04 09:54:05 +0100

    Multiples of 3 and 5 opdracht

---
+++ Multiples of 3 and 5/Multiples of 3 and 5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Multiples_of_3_and_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int result = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (((i % 3) == 0) || ((i % 5) == 0))
+                {
+                    Console.WriteLine(result += i);
+                }
+            }
+        }
+    }
+}
