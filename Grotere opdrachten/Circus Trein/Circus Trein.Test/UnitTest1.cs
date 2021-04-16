85d5ae8005f4d0481d17be6e88167828a74110f4
Tom Derks <459530@student.fontys.nl>
2021-04-16 20:58:06 +0200

    Unit test?

---
+++ Grotere opdrachten/Circus Trein/Circus Trein.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Circus_Trein.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
---
+++ Grotere opdrachten/Circus Trein/Circus_trein.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Circus_Trein.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
